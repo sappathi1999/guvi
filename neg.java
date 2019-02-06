8cafe688ac7fa232cf923e8e195434bfc03f4cf5
sappathi1999 <47376756+sappathi1999@users.noreply.github.com>
2019-02-06 01:38:21 -0800

    Create neg.java

---
+++ neg.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+      int a;
+     Scanner S= new Scanner(System.in);
+      System.out.println(" ");
+      a=S.nextInt();
+      if(a>0)
+        {
+        System.out.println("positive");
+      }
+        else
+        if(a==0)
+        {
+            System.out.println("zero");
+        }
+       else
+       {
+       	System.out.println("negative");
+        }
+    }
+}
