c88ae5f71729440b842d037c170bd5919fb70f0f
sappathi1999 <47376756+sappathi1999@users.noreply.github.com>
2019-02-06 00:53:43 -0800

    Create exam.java

---
+++ exam.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        int number=3;
+        if(3>0)
+        {
+            System.out.println("positive");
+        }
+        else if(3<0)
+        {
+            System.out.println("negative ");
+        }
+        else
+        {
+            System.out.println("positive or negative");
+        }
+    }
+}
